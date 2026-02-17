1b4b9b221045ade094df71c0e09d850905d5e968
WilliamYizima <william.yizima@hotmail.com>
2026-02-16 21:37:04 -0300

    Refactor Terraform configuration: separate variables, outputs, and provider into distinct files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+do_token     = "TOKEN_HERE"
+droplet_name = "teste-dev-02"
+# droplet_region=
+# droplet_size=
+# ssh_key_name=default
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+
+variable "do_token" {
+
+}
+
+variable "droplet_name" {
+  default = "teste-dev"
+}
+
+variable "droplet_region" {
+  default = "nyc1"
+}
+
+variable "droplet_size" {
+  default = "s-1vcpu-2gb"
+}
+
+variable "ssh_key_name" {
+  default = "ssh-teste-dev"
+}
